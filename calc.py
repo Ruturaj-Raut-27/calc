a768d0a8d61b98e36e94863d8be0e1c6d1bd6154
Ruturaj-Raut-27 <ruturajraut27@gmail.com>
2025-12-31 23:23:53 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,9 +2,11 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # Perform addition
-sum = num1 + num2
+addition = num1 + num2
+subtraction = num1 - num2
 multiplication = num1 * num2
 
 # Display the result
-print("Addition is:", sum)
+print("Addition is:", addition)
+print("Subtraction is:", subtraction)
 print("Multiplication is:", multiplication)
