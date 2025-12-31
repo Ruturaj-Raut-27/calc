94c8d5c44a340b97de995e93f62a8e2c186bced5
Ruturaj-Raut-27 <ruturajraut27@gmail.com>
2025-12-31 23:18:56 +0530

    added multiplication

--- calc.py
+++ calc.py
@@ -3,6 +3,8 @@ num2 = int(input("Enter second number: "))
 
 # Perform addition
 sum = num1 + num2
+multiplication = num1 * num2
 
 # Display the result
 print("Addition is:", sum)
+print("Multiplication is:", multiplication)
